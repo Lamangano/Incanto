9b3def91c11ac283828017d4909e97e3badd07f3
Lamangano <103276860+Lamangano@users.noreply.github.com>
2024-07-15 21:12:21 -0300

    anda el carrousel

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     'https://www.youtube.com/embed/huX_UBpn_JQ?si=pfOb_1snvcT5fy37',
     'https://www.youtube.com/embed/Ag6HQ7cwZ8g?si=CPqNEvkjKlTLgcNc',
     'https://www.youtube.com/embed/xVg7daj_RSE?si=3Ke-Q5UYhtpB1-iB',
-    'https://www.youtube.com/embed/5zq28UDpMvs?si=yXTFW6mZ2ZbQMzSi',
+    'https://www.youtube.com/embed/zY8rjVZYabo?si=upjuxYnA3J-2fasQ',
     'https://www.youtube.com/embed/_scyhkhAnSM?si=zNgN3oet7pRIvIZm',
     'https://www.youtube.com/embed/UD8eRneaU8s?si=IZTsVP_u7W_bifoo',
     'https://www.youtube.com/embed/eAJoQ3eXurM?si=zrBBZoN1CGMcOssZ',
